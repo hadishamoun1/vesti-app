de6e93d216f7a0adf7cb367cefaae733492992b3
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-23 20:04:54 +0300

    Created initial code for search stores

---
+++ lib/screens/SearchStores.dart
@@ -0,0 +1,16 @@
+// SearchStoresPage.dart
+import 'package:flutter/material.dart';
+
+class SearchStoresPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Stores'),
+      ),
+      body: Center(
+        child: Text('Search for stores here'),
+      ),
+    );
+  }
+}

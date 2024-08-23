2784fee258cd04483cdb00a4d3a1867fb6908071
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-23 17:40:37 +0300

    Created a container with price and add to cart button + view store button

--- lib/screens/ProductDetails.dart
+++ lib/screens/ProductDetails.dart
@@ -27,7 +27,6 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
         return Colors.orange;
       case 'purple':
         return Colors.purple;
-
       default:
         return Colors.grey;
     }
@@ -40,166 +39,238 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
         title: Text('Product Details'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 10),
-              Image.network(
-                widget.product['imageUrl'] ?? '',
-                height: 200,
-                width: double.infinity,
-                errorBuilder: (context, error, stackTrace) {
-                  return Icon(Icons.error, size: 100);
-                },
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  widget.product['name'] ?? 'Unknown Product',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Divider(
-                color: Colors.black,
-                thickness: 2,
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Description:\n\n',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 10),
+                    Image.network(
+                      widget.product['imageUrl'] ?? '',
+                      height: 200,
+                      width: double.infinity,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Icon(Icons.error, size: 100);
+                      },
                     ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: widget.product['description'] ??
-                            'No description available',
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        widget.product['name'] ?? 'Unknown Product',
                         style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            fontWeight: FontWeight.normal),
+                            fontSize: 24, fontWeight: FontWeight.bold),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4.0),
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Available Sizes:\n',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
                     ),
-                  ),
-                ),
-              ),
-              Wrap(
-                spacing: 8.0,
-                runSpacing: 8.0,
-                children: (widget.product['availableSizes'] as List<dynamic>?)
-                        ?.map((size) => Container(
-                              constraints: BoxConstraints(
-                                maxWidth: 50,
-                                minHeight: 50,
-                              ),
-                              child: GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    _selectedSize = size;
-                                  });
-                                  // Handle size selection
-                                  print('Selected size: $size');
-                                },
-                                child: Container(
-                                  padding: EdgeInsets.all(12),
-                                  decoration: BoxDecoration(
-                                    color: _selectedSize == size
-                                        ? Colors.blue
-                                        : Colors.grey[200], // Background color
-                                    borderRadius: BorderRadius.circular(25),
-                                    border: Border.all(
-                                      color: _selectedSize == size
-                                          ? Colors.blue
-                                          : Colors.transparent, // Border color
-                                      width: 2,
-                                    ),
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      size.toString(),
-                                      style: TextStyle(
-                                        color: _selectedSize == size
-                                            ? Colors.white
-                                            : Colors.black, // Text color
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ))
-                        .toList() ??
-                    [Text('No sizes available')],
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4.0),
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Available Colors:\n',
-                    style: TextStyle(
-                      fontSize: 18,
+                    Divider(
                       color: Colors.black,
-                      fontWeight: FontWeight.bold,
+                      thickness: 2,
                     ),
-                  ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Description:\n\n',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: widget.product['description'] ??
+                                  'No description available',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Available Sizes:\n',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 8.0,
+                      children:
+                          (widget.product['availableSizes'] as List<dynamic>?)
+                                  ?.map((size) => Container(
+                                        constraints: BoxConstraints(
+                                          maxWidth: 50,
+                                          minHeight: 50,
+                                        ),
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            setState(() {
+                                              _selectedSize = size;
+                                            });
+                                            // Handle size selection
+                                            print('Selected size: $size');
+                                          },
+                                          child: Container(
+                                            padding: EdgeInsets.all(12),
+                                            decoration: BoxDecoration(
+                                              color: _selectedSize == size
+                                                  ? Colors.blue
+                                                  : Colors.grey[
+                                                      200], // Background color
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                              border: Border.all(
+                                                color: _selectedSize == size
+                                                    ? Colors.blue
+                                                    : Colors
+                                                        .transparent, // Border color
+                                                width: 2,
+                                              ),
+                                            ),
+                                            child: Center(
+                                              child: Text(
+                                                size.toString(),
+                                                style: TextStyle(
+                                                  color: _selectedSize == size
+                                                      ? Colors.white
+                                                      : Colors
+                                                          .black, // Text color
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ))
+                                  .toList() ??
+                              [Text('No sizes available')],
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Available Colors:\n',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 8.0,
+                      children:
+                          (widget.product['availableColors'] as List<dynamic>?)
+                                  ?.map((colorName) => GestureDetector(
+                                        onTap: () {
+                                          setState(() {
+                                            _selectedColor = colorName;
+                                          });
+                                          // Handle color selection
+                                          print('Selected color: $colorName');
+                                        },
+                                        child: Container(
+                                          width: 40,
+                                          height: 40,
+                                          decoration: BoxDecoration(
+                                            color: _getColorFromName(
+                                                colorName), // Use mapped color
+                                            borderRadius:
+                                                BorderRadius.circular(25),
+                                            border: Border.all(
+                                              color: _selectedColor == colorName
+                                                  ? Colors.white
+                                                  : Colors
+                                                      .transparent, // Border color
+                                              width: 2,
+                                            ),
+                                          ),
+                                        ),
+                                      ))
+                                  .toList() ??
+                              [Text('No colors available')],
+                    ),
+                  ],
                 ),
               ),
-              Wrap(
-                spacing: 8.0,
-                runSpacing: 8.0,
-                children: (widget.product['availableColors'] as List<dynamic>?)
-                        ?.map((colorName) => GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  _selectedColor = colorName;
-                                });
-                                // Handle color selection
-                                print('Selected color: $colorName');
-                              },
-                              child: Container(
-                                width: 40,
-                                height: 40,
-                                decoration: BoxDecoration(
-                                  color: _getColorFromName(
-                                      colorName), // Use mapped color
-                                  borderRadius: BorderRadius.circular(25),
-                                  border: Border.all(
-                                    color: _selectedColor == colorName
-                                        ? Colors.white
-                                        : Colors.transparent, // Border color
-                                    width: 2,
-                                  ),
-                                ),
-                              ),
-                            ))
-                        .toList() ??
-                    [Text('No colors available')],
-              ),
-            ],
+            ),
           ),
-        ),
+          Container(
+            height: 160,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white, // White background color
+              borderRadius: BorderRadius.circular(30), // Border radius
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2), // Shadow color
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0, 4), // Shadow position
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.end, // Align content to the bottom
+              crossAxisAlignment:
+                  CrossAxisAlignment.stretch, // Stretch buttons to full width
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Price: \$${widget.product['price'] ?? '0.00'}',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Handle button press
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue, 
+                        foregroundColor: Colors.white, 
+                      ),
+                      child: Text('Add to cart'),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle "View Store" button press
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue, 
+                    foregroundColor: Colors.white, 
+                  ),
+                  child: Text('View Store'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }

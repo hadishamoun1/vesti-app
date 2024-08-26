4d702b6912c3088e6d47b63a7580031fcb707ed6
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-26 14:38:00 +0300

    Added search bar code to file

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 
 class SearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final Function(String) onChanged;
+  final String hintText;
+
+  SearchBar({
+    required this.controller,
+    required this.onChanged,
+    required this.hintText,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.fromLTRB(16, 5, 16, 16),
       child: TextField(
+        controller: controller,
+        onChanged: onChanged,
         decoration: InputDecoration(
-          hintText: 'Search products...',
-          prefixIcon: Icon(Icons.search),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey[500]),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.grey[500],
           ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(
+              color: Colors.grey[300]!,
+              width: 1.5,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(
+              color: Colors.grey,
+              width: 1.5,
+            ),
+          ),
+          contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
         ),
-        onChanged: (value) {
-          
-        },
       ),
     );
   }

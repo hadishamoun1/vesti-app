89960ea9d84b1bcff6202e1ac26faed5e3fb9f17
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-26 14:35:38 +0300

    Added product grid code to file

---
+++ lib/widgets/product_grid.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import '../screens/ProductDetails.dart'; // Ensure you have this import
+
+class ProductGrid extends StatelessWidget {
+  final List<dynamic> products;
+
+  ProductGrid({required this.products});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 11.0),
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+          childAspectRatio: 0.7,
+        ),
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProductDetailsPage(
+                    product: product,
+                  ),
+                ),
+              );
+            },
+            child: Card(
+              elevation: 2,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(8.0)),
+                      child: Image.network(
+                        product['imageUrl'] ?? '', // Use appropriate key for image URL
+                        width: double.infinity,
+                        height: double.infinity,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Icon(Icons.error, size: 100);
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 1.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        product['name'] ?? 'Unknown Product',
+                        style: TextStyle(fontSize: 15),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

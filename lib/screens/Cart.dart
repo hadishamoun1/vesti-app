1490bcf40e9822bc459eb37ffa4ddee698fe30b3
hadishamoun1 <shamounhadi2@gmail.com>
2024-08-27 20:02:55 +0300

    Added hardcoded cards for testing

--- lib/screens/Cart.dart
+++ lib/screens/Cart.dart
@@ -6,6 +6,11 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+    List<CartItem> cartItems = [
+    CartItem(name: 'T-Shirt', price: 20.0, quantity: 1),
+    CartItem(name: 'Jeans', price: 40.0, quantity: 2),
+    CartItem(name: 'Sneakers', price: 60.0, quantity: 1),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(

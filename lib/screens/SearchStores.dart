b6f28f4957841622aba42cca497ae2d1f19e66df
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-03 15:12:39 +0300

    Added path for ai search screen in search stores screen

--- lib/screens/SearchStores.dart
+++ lib/screens/SearchStores.dart
@@ -6,6 +6,7 @@ import '../widgets/bottom_nav_bar.dart';
 import 'dart:convert';
 import '../widgets/custom_search_bar.dart';
 import 'package:geolocator/geolocator.dart';
+import 'AiSearch.dart';
 
 var primaryColor = Color.fromARGB(255, 255, 255, 255);
 var secondaryColor = Color(0xFF3882cd);
@@ -113,116 +114,108 @@ class _SearchStoresPageState extends State<SearchStoresPage> {
   }
 
   void _onTap(int index) {
-    if (_currentIndex != index) {
-      setState(() {
-        _currentIndex = index;
-      });
+    setState(() {
+      _currentIndex = index;
+    });
 
+    if (index == 1) {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(
-          builder: (context) {
-            switch (index) {
-              case 0:
-                return HomePage();
-              case 1:
-                return SearchStoresPage();
-              default:
-                return SearchStoresPage();
-            }
-          },
-        ),
+        MaterialPageRoute(builder: (context) => SearchStoresPage()),
+      );
+    } else if (index == 0) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } else if (index == 2) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SearchScreen()),
       );
     }
   }
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Colors.white, // Background color similar to Scaffold
-    body: Column(
-      children: [
-        // AppBar replacement
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          color: Theme.of(context).primaryColor,
-          child: Center(
-            child: Text(
-              'Search Stores',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(height: 10),
-        CustomSearchBar(
-          hintText: 'Search...',
-          onChanged: (query) {
-            _searchStores(query);
-          },
-        ),
-        SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: _getCurrentLocation,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: _showNearbyStores ? secondaryColor : primaryColor,
-                foregroundColor: _showNearbyStores ? primaryColor : Colors.black,
-                side: BorderSide(color: borderColor, width: 1),
-              ),
-              child: Text('View Nearby Stores'),
-            ),
-            OutlinedButton(
-              onPressed: _fetchAllStores,
-              style: OutlinedButton.styleFrom(
-                backgroundColor: !_showNearbyStores ? secondaryColor : primaryColor,
-                foregroundColor: !_showNearbyStores ? primaryColor : Colors.black,
-                side: BorderSide(color: borderColor, width: 1),
-              ),
-              child: Text('View All Stores'),
-            ),
-          ],
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: _filteredStores.length,
-            itemBuilder: (context, index) {
-              final store = _filteredStores[index];
-              final storeName = store['name'] ?? 'Unnamed Store';
-              final pictureURL = store['pictureURL'];
 
-              return ListTile(
-                leading: pictureURL != null && pictureURL.isNotEmpty
-                    ? Image.network(
-                        pictureURL,
-                        width: 50,
-                        height: 50,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) =>
-                            Icon(Icons.error),
-                      )
-                    : Icon(Icons.store),
-                title: Text(storeName),
-                subtitle: Text(store['description'] ?? 'No description available'),
-              );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Stores'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          CustomSearchBar(
+            hintText: 'Search...',
+            onChanged: (query) {
+              _searchStores(query);
             },
           ),
-        ),
-      ],
-    ),
-    bottomNavigationBar: BottomNavBar(
-      currentIndex: _currentIndex,
-      onTap: (index) {
-        _onTap(index);
-      },
-    ),
-  );
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // Use ElevatedButton for the selected state
+              ElevatedButton(
+                onPressed: _getCurrentLocation,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      _showNearbyStores ? secondaryColor : primaryColor,
+                  foregroundColor:
+                      _showNearbyStores ? primaryColor : Colors.black,
+                  side: BorderSide(color: borderColor, width: 1),
+                ),
+                child: Text('View Nearby Stores'),
+              ),
+              // Use OutlinedButton for the unselected state
+              OutlinedButton(
+                onPressed: _fetchAllStores,
+                style: OutlinedButton.styleFrom(
+                  backgroundColor:
+                      !_showNearbyStores ? secondaryColor : primaryColor,
+                  foregroundColor:
+                      !_showNearbyStores ? primaryColor : Colors.black,
+                  side: BorderSide(color: borderColor, width: 1),
+                ),
+                child: Text('View All Stores'),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredStores.length,
+              itemBuilder: (context, index) {
+                final store = _filteredStores[index];
+
+                final storeName = store['name'] ?? 'Unnamed Store';
+                final pictureURL = store['pictureURL'];
+
+                return ListTile(
+                  leading: pictureURL != null && pictureURL.isNotEmpty
+                      ? Image.network(
+                          pictureURL,
+                          width: 50,
+                          height: 50,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              Icon(Icons.error),
+                        )
+                      : Icon(Icons.store),
+                  title: Text(storeName),
+                  subtitle:
+                      Text(store['description'] ?? 'No description available'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          _onTap(index);
+        },
+      ),
+    );
+  }
 }
-
-}
-
-

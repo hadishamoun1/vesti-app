c02d95351ec74531b9455b006585bcab80b6699f
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-03 18:18:19 +0300

    Changed background color of profile page to white

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -1,5 +1,5 @@
-import 'package:app/widgets/DashedDivider.dart';
 import 'package:flutter/material.dart';
+import 'package:app/widgets/DashedDivider.dart'; // Import the DashedDivider widget
 import '../widgets/bottom_nav_bar.dart';
 import 'Home.dart';
 import 'AiSearch.dart';
@@ -51,91 +51,97 @@ class _ProfilePageState extends State<ProfilePage> {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 150,
-            padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-            color: Colors.white,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 40,
-                      backgroundImage:
-                          NetworkImage('https://via.placeholder.com/150'),
-                      backgroundColor: Colors.white,
-                    ),
-                    SizedBox(width: 16),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 8),
-                        Text(
-                          'John Doe',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+      body: Container(
+        color: Colors.white, 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 40,
+                        backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 8),
+                          Text(
+                            'John Doe',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 6),
-                        Text(
-                          'john.doe@example.com',
-                          style: TextStyle(
-                            color: Colors.black26,
-                            fontSize: 16,
+                          SizedBox(height: 6),
+                          Text(
+                            'john.doe@example.com',
+                            style: TextStyle(
+                              color: Colors.black26,
+                              fontSize: 16,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                        ],
+                      ),
+                    ],
+                  ),
+                  
+                  
+                ],
+              ),
             ),
-          ),
-          DashedDivider( width: MediaQuery.of(context).size.width, 
-                  alignment: Alignment.bottomCenter,),
-          SizedBox(height: 16),
-          Expanded(
-            child: ListView(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.notifications, color: secondaryColor),
-                  title: Text('Notifications'),
-                  onTap: () {
-                    //Navigator.push(
-                    //context,
-                    // MaterialPageRoute(builder: (context) => NotificationsPage()),
-                    //);
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.favorite, color: Colors.red),
-                  title: Text('Favorites'),
-                  onTap: () {
-                    //Navigator.push(
-                    //context,
-                    //MaterialPageRoute(builder: (context) => FavoritesPage()),
-                    //);
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.list, color: secondaryColor),
-                  title: Text('Orders'),
-                  onTap: () {
-                    //Navigator.push(
-                    //context,
-                    //MaterialPageRoute(builder: (context) => OrdersPage()),
-                    //);
-                  },
-                ),
-              ],
+            DashedDivider(
+                    width: MediaQuery.of(context).size.width, // Ensure full width
+                    alignment: Alignment.bottomCenter,
+                  ),
+            SizedBox(height: 16),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.notifications, color: secondaryColor),
+                    title: Text('Notifications'),
+                    onTap: () {
+                      //Navigator.push(
+                      //context,
+                      // MaterialPageRoute(builder: (context) => NotificationsPage()),
+                      //);
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.favorite, color: Colors.red),
+                    title: Text('Favorites'),
+                    onTap: () {
+                      //Navigator.push(
+                      //context,
+                      //MaterialPageRoute(builder: (context) => FavoritesPage()),
+                      //);
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.list, color: secondaryColor),
+                    title: Text('Orders'),
+                    onTap: () {
+                      //Navigator.push(
+                      //context,
+                      //MaterialPageRoute(builder: (context) => OrdersPage()),
+                      //);
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavBar(
         currentIndex: _currentIndex,

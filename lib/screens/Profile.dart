e9a51ca569578e789685a96ab67b5d44875d6e3f
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-03 16:19:12 +0300

    Created a basic code for profile screen

---
+++ lib/screens/Profile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Colors.blueGrey[900],
+        elevation: 0,
+      ),
+    );
+  }
+}

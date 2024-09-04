1c17114ca4ac117df92abb5c81646a6eb3fbce15
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-04 21:29:20 +0300

    Chnaged card border radius and product name to normaol font

--- lib/screens/StoreDetails.dart
+++ lib/screens/StoreDetails.dart
@@ -8,7 +8,8 @@ class StoreDetailsPage extends StatelessWidget {
   StoreDetailsPage({required this.store});
 
   Future<List<dynamic>> fetchProducts() async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:3000/products/store/${store['id']}'));
+    final response = await http
+        .get(Uri.parse('http://10.0.2.2:3000/products/store/${store['id']}'));
 
     if (response.statusCode == 200) {
       return json.decode(response.body);
@@ -77,24 +78,73 @@ class StoreDetailsPage extends StatelessWidget {
                   return Center(child: Text('No products available'));
                 } else {
                   final products = snapshot.data!;
-                  return ListView.builder(
+                  return GridView.builder(
                     shrinkWrap: true,
                     physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 8.0,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.9,
+                    ),
                     itemCount: products.length,
                     itemBuilder: (context, index) {
                       final product = products[index];
-                      return ListTile(
-                        leading: Image.network(
-                          product['imageUrl'],
-                          fit: BoxFit.cover,
-                          width: 50,
-                          height: 50,
-                          errorBuilder: (context, error, stackTrace) {
-                            return Icon(Icons.error, color: Colors.red);
-                          },
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                        title: Text(product['name']),
-                        subtitle: Text('\$${product['price']}'),
+                        elevation: 5,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(15.0)),
+                                child: Image.network(
+                                  product['imageUrl'],
+                                  fit: BoxFit.fill,
+                                  width: double.infinity,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return Center(
+                                      child: Icon(
+                                        Icons.error,
+                                        color: Colors.red,
+                                        size: 50,
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                product['name'],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 16,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: Text(
+                                '\$${product['price']}',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                          ],
+                        ),
                       );
                     },
                   );

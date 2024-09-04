457dadae295698b74518aea089d4b9edab3a9150
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-04 20:04:49 +0300

    Added padding and border radius to image in store details

--- lib/screens/StoreDetails.dart
+++ lib/screens/StoreDetails.dart
@@ -13,30 +13,37 @@ class StoreDetailsPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-              height: 250,
-              child: Image.network(
-                store['pictureUrl'],
-                fit: BoxFit.cover,
-                errorBuilder: (BuildContext context, Object error,
-                    StackTrace? stackTrace) {
-                  return Center(
-                    child: Icon(
-                      Icons.error,
-                      color: Colors.red,
-                      size: 50,
-                    ),
-                  );
-                },
-              ),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0), 
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15), 
+          child: Container(
+            width: double.infinity,
+            height: 250,
+            child: Image.network(
+              store['pictureUrl'],
+              fit: BoxFit.cover,
+              errorBuilder: (BuildContext context, Object error,
+                  StackTrace? stackTrace) {
+                return Center(
+                  child: Icon(
+                    Icons.error,
+                    color: Colors.red,
+                    size: 50,
+                  ),
+                );
+              },
             ),
-          ],
+          ),
         ),
       ),
+    ],
+  ),
+),
+
     );
   }
 }

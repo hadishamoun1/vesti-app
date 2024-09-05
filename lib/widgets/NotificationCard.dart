0e3789c1266b805000706bdc0eba15a2f790721d
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 19:54:43 +0300

    Created basic notification card code

---
+++ lib/widgets/NotificationCard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotificationCard extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String message;
+
+  NotificationCard({
+    required this.imageUrl,
+    required this.name,
+    required this.message,
+  });
+}

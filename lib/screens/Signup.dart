22a8f0893e5d9f9c562bf1df7307432b3c8aafc5
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 14:08:10 +0300

    Created the email textfield

--- lib/screens/Signup.dart
+++ lib/screens/Signup.dart
@@ -7,6 +7,15 @@ class SignupPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Sign Up'),
       ),
+       body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+        ])
+       )
     );
   }
 }

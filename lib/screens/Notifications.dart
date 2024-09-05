34ea0ed9c8be4581a0a7615061c9bbb208156c91
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 20:01:32 +0300

    Added starting code

---
+++ lib/screens/Notifications.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/NotificationCard.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  // This list will store the static notifications data
+  final List<NotificationItem> _notifications = [
+    NotificationItem(
+      imageUrl: 'https://via.placeholder.com/50',
+      name: 'Store Name',
+      message: 'This is a sample notification message from the store.',
+    ),
+    NotificationItem(
+      imageUrl: 'https://via.placeholder.com/50',
+      name: 'Another Store',
+      message: 'Another sample notification message from another store.',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+      ),
+    );
+  }
+}

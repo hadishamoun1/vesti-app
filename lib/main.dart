a3c677f1e50b7f4727cd31a1fe364f7fc63a46c4
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 14:21:17 +0300

    Removed initial from main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       initialRoute: '/login',
+       
        routes: {
         '/login': (context) => LoginPage(),
         '/signup': (context) => SignupPage(),

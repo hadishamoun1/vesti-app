e51c9b89264ad501178c1baa90aec2d8108abf26
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 21:22:34 +0300

    Created order model

---
+++ lib/widgets/order_cards.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OrderCard extends StatelessWidget {
+  final Order order;
+
+  OrderCard({required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(16),
+        title: Text('Order #${order.id}'),
+        subtitle: Text('Status: ${order.status}'),
+      ),
+    );
+  }
+}

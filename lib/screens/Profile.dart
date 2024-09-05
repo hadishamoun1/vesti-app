bd01dfa3342615362b36279f9aedc7f2f664be81
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 18:10:20 +0300

    Created the signout button

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // Import shared_preferences
 import 'package:app/widgets/DashedDivider.dart';
-
 
 var secondaryColor = Color(0xFF3882cd);
 
@@ -10,97 +10,122 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  // Function to handle sign out
+  Future<void> _signOut() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('jwt_token'); // Remove the token from shared preferences
+
+    // Navigate to login page after sign out
+    Navigator.pushReplacementNamed(context, '/login');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 150,
-            padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-            color: Colors.white,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 40,
-                      backgroundImage:
-                          NetworkImage('https://via.placeholder.com/150'),
-                      backgroundColor: Colors.white,
-                    ),
-                    SizedBox(width: 16),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 8),
-                        Text(
-                          'John Doe',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 40,
+                        backgroundImage:
+                            NetworkImage('https://via.placeholder.com/150'),
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 8),
+                          Text(
+                            'John Doe',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        SizedBox(height: 6),
-                        Text(
-                          'john.doe@example.com',
-                          style: TextStyle(
-                            color: Colors.black26,
-                            fontSize: 16,
+                          SizedBox(height: 6),
+                          Text(
+                            'john.doe@example.com',
+                            style: TextStyle(
+                              color: Colors.black26,
+                              fontSize: 16,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            DashedDivider(
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.bottomCenter,
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.notifications, color: secondaryColor),
+                    title: Text('Notifications'),
+                    onTap: () {
+                      // Navigate to notifications page
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.favorite, color: Colors.red),
+                    title: Text('Favorites'),
+                    onTap: () {
+                      // Navigate to favorites page
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.list, color: secondaryColor),
+                    title: Text('Orders'),
+                    onTap: () {
+                      // Navigate to orders page
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.center, // Centers the button horizontally
+                child: FractionallySizedBox(
+                  widthFactor: 0.5, // 50% of the screen width
+                  child: ElevatedButton(
+                    onPressed: _signOut,
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      backgroundColor: secondaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
-                  ],
+                    child: Text(
+                      'Sign Out',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-          DashedDivider(
-            width: MediaQuery.of(context).size.width,
-            alignment: Alignment.bottomCenter,
-          ),
-          SizedBox(height: 16),
-          Expanded(
-            child: ListView(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.notifications, color: secondaryColor),
-                  title: Text('Notifications'),
-                  onTap: () {
-                    //Navigator.push(
-                    //context,
-                    // MaterialPageRoute(builder: (context) => NotificationsPage()),
-                    //);
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.favorite, color: Colors.red),
-                  title: Text('Favorites'),
-                  onTap: () {
-                    //Navigator.push(
-                    //context,
-                    //MaterialPageRoute(builder: (context) => FavoritesPage()),
-                    //);
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.list, color: secondaryColor),
-                  title: Text('Orders'),
-                  onTap: () {
-                    //Navigator.push(
-                    //context,
-                    //MaterialPageRoute(builder: (context) => OrdersPage()),
-                    //);
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

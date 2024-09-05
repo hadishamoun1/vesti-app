97df975b97242a9e0099f80f003c3ca1930fa269
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 14:16:43 +0300

    Added route to signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/Home.dart';
 import 'screens/Login.dart';
+import 'screens/Signup.dart';
 
 void main() async {
 
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
        initialRoute: '/login',
        routes: {
         '/login': (context) => LoginPage(),
+        '/signup': (context) => SignupPage(),
         
       },
       home: HomePage(),

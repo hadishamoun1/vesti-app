989166c38ad2440717ada6d99573a300ec9d1ea7
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-05 19:57:13 +0300

    Added image to card

--- lib/widgets/NotificationCard.dart
+++ lib/widgets/NotificationCard.dart
@@ -9,5 +9,36 @@ class NotificationCard extends StatelessWidget {
     required this.imageUrl,
     required this.name,
     required this.message,
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 4,
+      child: ListTile(
+        contentPadding: EdgeInsets.all(8),
+        leading: ClipOval(
+          child: Image.network(
+            imageUrl,
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
+          ),
+        ),
+        title: Text(
+          name,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    ),
+  }
+
   });
 }

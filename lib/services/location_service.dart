715fc22c668e4296d348689e804142a6f9b75a73
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-06 15:16:19 +0300

    Added continous updates of location

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,5 +1,6 @@
 import 'package:geolocator/geolocator.dart';
 
+// One-time location fetch
 Future<Position> getUserLocation() async {
   bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
@@ -15,4 +16,23 @@ Future<Position> getUserLocation() async {
   }
 
   return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+}
+
+// Continuous location updates
+Stream<Position> getLocationUpdates() {
+  return Geolocator.getPositionStream(
+    locationSettings: LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 10, // Get updates after moving 10 meters
+    ),
+  );
+}
+
+void listenToLocationUpdates() {
+  Stream<Position> positionStream = getLocationUpdates();
+
+  positionStream.listen((Position position) {
+    print('Updated User Location: ${position.latitude}, ${position.longitude}');
+    
+  });
 }

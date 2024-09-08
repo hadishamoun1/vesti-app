9771abe58531eb2fcee8cc680bc00b55897cbe9b
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-08 03:39:43 +0300

    Created startup code for checkout

---
+++ lib/screens/Checkout.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class PaymentScreen extends StatefulWidget {
+  @override
+  _PaymentScreenState createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  final TextEditingController _addressController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Payment'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Address',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _addressController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your address',
+              ),
+              maxLines: 5, // Allows multiple lines of text
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

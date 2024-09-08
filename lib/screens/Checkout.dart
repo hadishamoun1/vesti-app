942a78fb7056c16e195cba7f3b1df5fbe26634f5
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-08 23:28:44 +0300

    Fixed bug subdomain and map location to beirut

--- lib/screens/Checkout.dart
+++ lib/screens/Checkout.dart
@@ -2,7 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-class OpenStreetMapScreen extends StatelessWidget {
+class OpenStreetMapScreen extends StatefulWidget {
+  @override
+  _OpenStreetMapScreenState createState() => _OpenStreetMapScreenState();
+}
+
+class _OpenStreetMapScreenState extends State<OpenStreetMapScreen> {
+  LatLng _markerPosition = LatLng(33.8938, 35.5018);
+
+  void _onTap(LatLng point) {
+    setState(() {
+      _markerPosition = point;
+    });
+    print('Tapped position: $point');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,25 +25,20 @@ class OpenStreetMapScreen extends StatelessWidget {
       ),
       body: FlutterMap(
         options: MapOptions(
-          initialCenter: LatLng(33.8886, 35.4955),
+          initialCenter: _markerPosition,
           initialZoom: 13.0,
-          onTap: (tapPosition, point) {
-            print('Tapped position: $point');
-          },
+          onTap: (tapPosition, point) => _onTap(point),
         ),
         children: [
           TileLayer(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c'],
-            // Attribution text can be added here directly
-            // FlutterMap should handle the attribution automatically
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           ),
           MarkerLayer(
             markers: [
               Marker(
-                point: LatLng(33.8886, 35.4955),
                 width: 80.0,
                 height: 80.0,
+                point: _markerPosition,
                 child: Container(
                   child: Icon(
                     Icons.location_on,

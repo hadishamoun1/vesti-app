f40158547c820767226ad25dbfc8cf85a0a3c75c
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-09 15:15:45 +0300

    Made the screen scrollable and fixed image width and height

--- lib/screens/Checkout.dart
+++ lib/screens/Checkout.dart
@@ -16,8 +16,11 @@ class PaymentScreen extends StatefulWidget {
 
 class _PaymentScreenState extends State<PaymentScreen> {
   LatLng _markerPosition = LatLng(33.8938, 35.5018);
+
+  // Track the selected payment method
   String _selectedPaymentMethod = '';
 
+  // Function to set the selected payment method
   void _selectPaymentMethod(String method) {
     setState(() {
       _selectedPaymentMethod = method;
@@ -33,137 +36,193 @@ class _PaymentScreenState extends State<PaymentScreen> {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(16, 16, 0, 5),
-              child: Text(
-                'Delivery Address',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => OpenStreetMapScreen(),
-                    ),
-                  );
-                },
-                child: Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        offset: Offset(0, 2),
-                        blurRadius: 6,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 16, 0, 5),
+                      child: Text(
+                        'Delivery Address',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
                       ),
-                    ],
+                    ),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => OpenStreetMapScreen(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              offset: Offset(0, 2),
+                              blurRadius: 6,
+                            ),
+                          ],
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            'assets/images/imageMaps.png',
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 16, 0, 5),
+                      child: Text(
+                        'Payment Method',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.credit_card, color: Colors.blue),
+                          title: Text('Credit / Debit Card'),
+                          trailing: _selectedPaymentMethod == 'card'
+                              ? Icon(Icons.check_circle, color: Colors.green)
+                              : null,
+                          selected: _selectedPaymentMethod == 'card',
+                          onTap: () {
+                            _selectPaymentMethod('card');
+                          },
+                        ),
+                        Divider(),
+                        ListTile(
+                          leading: Icon(Icons.account_balance_wallet,
+                              color: Colors.orange),
+                          title: Text('PayPal'),
+                          trailing: _selectedPaymentMethod == 'paypal'
+                              ? Icon(Icons.check_circle, color: Colors.green)
+                              : null,
+                          selected: _selectedPaymentMethod == 'paypal',
+                          onTap: () {
+                            _selectPaymentMethod('paypal');
+                          },
+                        ),
+                        Divider(),
+                        ListTile(
+                          leading:
+                              Icon(Icons.attach_money, color: Colors.green),
+                          title: Text('Cash on Delivery'),
+                          trailing: _selectedPaymentMethod == 'cash'
+                              ? Icon(Icons.check_circle, color: Colors.green)
+                              : null,
+                          selected: _selectedPaymentMethod == 'cash',
+                          onTap: () {
+                            _selectPaymentMethod('cash');
+                          },
+                        ),
+                      ],
+                    ),
+                    if (_selectedPaymentMethod == 'card')
+                      _buildCardPaymentFields(),
+                    if (_selectedPaymentMethod == 'paypal')
+                      _buildPayPalPaymentFields(),
+                    if (_selectedPaymentMethod == 'cash')
+                      _buildCashOnDeliveryMessage(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 160,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 10,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: Text(
+                    'Total Items: ${widget.totalItems}',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(
-                      'assets/images/imageMaps.png',
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total: ',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '\$${widget.totalPrice.toStringAsFixed(2)}',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Handle checkout action
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      fixedSize: Size(400, 45),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 9),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      'Complete Payment',
+                      style: TextStyle(fontSize: 21),
                     ),
                   ),
-                ),
-              ),
+                )
+              ],
             ),
-            SizedBox(height: 30),
-            Padding(
-              padding: EdgeInsets.fromLTRB(16, 16, 0, 5),
-              child: Text(
-                'Payment Method',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.credit_card, color: Colors.blue),
-                    title: Text('Credit / Debit Card'),
-                    trailing: _selectedPaymentMethod == 'card'
-                        ? Icon(Icons.check_circle, color: Colors.green)
-                        : null,
-                    selected: _selectedPaymentMethod == 'card',
-                    onTap: () {
-                      _selectPaymentMethod('card');
-                    },
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.account_balance_wallet,
-                        color: Colors.orange),
-                    title: Text('PayPal'),
-                    trailing: _selectedPaymentMethod == 'paypal'
-                        ? Icon(Icons.check_circle, color: Colors.green)
-                        : null,
-                    selected: _selectedPaymentMethod == 'paypal',
-                    onTap: () {
-                      _selectPaymentMethod('paypal');
-                    },
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.attach_money, color: Colors.green),
-                    title: Text('Cash on Delivery'),
-                    trailing: _selectedPaymentMethod == 'cash'
-                        ? Icon(Icons.check_circle, color: Colors.green)
-                        : null,
-                    selected: _selectedPaymentMethod == 'cash',
-                    onTap: () {
-                      _selectPaymentMethod('cash');
-                    },
-                  ),
-                ],
-              ),
-            ),
-            if (_selectedPaymentMethod == 'card') _buildCardPaymentFields(),
-            if (_selectedPaymentMethod == 'paypal') _buildPayPalPaymentFields(),
-            if (_selectedPaymentMethod == 'cash') _buildCashOnDeliveryMessage(),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Total Items: ${widget.totalItems}',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Total Price: \$${widget.totalPrice.toStringAsFixed(2)}',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

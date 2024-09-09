eb93c65cd9098586f9cee8a43c0d081173be506d
hadishamoun1 <shamounhadi2@gmail.com>
2024-09-09 17:06:08 +0300

    Added a gps icon

--- lib/widgets/OpenStreetMap.dart
+++ lib/widgets/OpenStreetMap.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
 class OpenStreetMapScreen extends StatefulWidget {
   @override
@@ -9,6 +10,50 @@ class OpenStreetMapScreen extends StatefulWidget {
 
 class _OpenStreetMapScreenState extends State<OpenStreetMapScreen> {
   LatLng _markerPosition = LatLng(33.8938, 35.5018);
+  final Location _location = Location();
+  bool _serviceEnabled = false;
+  PermissionStatus _permissionGranted = PermissionStatus.denied;
+  MapController _mapController = MapController();
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPermissions();
+  }
+
+  Future<void> _checkPermissions() async {
+    _serviceEnabled = await _location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await _location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      final locationData = await _location.getLocation();
+      if (locationData.latitude != null && locationData.longitude != null) {
+        setState(() {
+          _markerPosition =
+              LatLng(locationData.latitude!, locationData.longitude!);
+        });
+        _mapController.move(
+            _markerPosition, 13.0); // Move the map to the new position
+      }
+    } catch (e) {
+      print("Error getting location: $e");
+    }
+  }
 
   void _onTap(LatLng point) {
     setState(() {
@@ -22,8 +67,17 @@ class _OpenStreetMapScreenState extends State<OpenStreetMapScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('OpenStreetMap'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.my_location),
+            onPressed: () {
+              _getCurrentLocation();
+            },
+          ),
+        ],
       ),
       body: FlutterMap(
+        mapController: _mapController,
         options: MapOptions(
           initialCenter: _markerPosition,
           initialZoom: 13.0,
